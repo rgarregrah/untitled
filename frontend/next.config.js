a313dcb7e7116d5d2dd27c8a4229ab90d12ce981
rgarregrah <n948224373@gmail.com>
2022-08-28 00:17:25 +0900

    fix setting

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  // ホットリロード効かない問題
+  // @see https://github.com/vercel/next.js/issues/36774
+  webpack: (config, context) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
